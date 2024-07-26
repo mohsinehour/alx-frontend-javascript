5e233ce5c88a8155537f1766c1f7d368c0528fdf
mohsinehour <hourmatallahmohsine@gmail.com>
2024-07-26 17:07:50 +0100

    update

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,20 @@
 export default class Building {
-  constructor(sqft){
-    if (this.constructor !== Building && !this.evacuationWarningMessage) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
     }
-    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
   }
 
-  set sqft(sqft) {
-    this._sqft = sqft;
+  set sqft(value) {
+    this._sqft = value;
   }
 }
