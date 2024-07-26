86ba269ef9a71ded8e036b4ad011401e4b0974b5
mohsinehour <hourmatallahmohsine@gmail.com>
2024-07-26 16:45:43 +0100

    update

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,4 +1,4 @@
-import Classroom from './classroom.js';
+import Classroom from './0-classroom.js';
 
 export default function initializeRooms() {
   const rooms = [new Classroom(19), new Classroom(20), new Classroom(34)];
